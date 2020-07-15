6b160f2f8d91f8c34a5adae65b1fa0e2c471cd6c
Kate Skorija <kateskorija@gmail.com>
2020-07-15 11:23:16 -0700

    debug and finish
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>
    Co-authored-by: RichterBros <erich.richter@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,29 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ToDoList.Models;
 using System;
-using System.Collections.Generic;
 
-namespace ToDoList.Test
+namespace ToDoList.Tests
 {
   [TestClass]
   public class ItemTests : IDisposable
   {
+
     public void Dispose()
     {
       Item.ClearAll();
     }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
       Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
+
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       string description = "Walk the dog.";
-      Item newItem = new Item(description);
 
+      Item newItem = new Item(description);
       string result = newItem.Description;
 
       Assert.AreEqual(description, result);
@@ -55,17 +58,15 @@ namespace ToDoList.Test
     [TestMethod]
     public void GetAll_ReturnsItems_ItemList()
     {
-      string description01 = "walk the dog";
-      string description02 = "wash the dishes";
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
       Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item>{ newItem1, newItem2 };
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
       List<Item> result = Item.GetAll();
 
       CollectionAssert.AreEqual(newList, result);
     }
-
-    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
   }
 }
